c0b241980d132c10134b6808537a9d5e72976c0f
otangid <dikizulkarnaen021@gmail.com>
2026-09-06 16:36:01 +0700

    Maven Publish

--- crashreport/build.gradle.kts
+++ crashreport/build.gradle.kts
@@ -1,7 +1,10 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.publish)
 }
 
 android {
@@ -32,4 +35,44 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = "io.github.otangid",
+        artifactId = "CrashReportCompose",
+        version = "1.0"
+    )
+
+    pom {
+        name.set("CrashReportCompose Library")
+        description.set("A library for capturing and displaying a Compose-based crash report UI.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/otangid/CrashReportCompose")
+
+        licenses {
+            license {
+                name.set("The Apache Software License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("otangid")
+                name.set("Diki Zulkarnaen")
+                email.set("dikizulkarnaen021@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/otangid/CrashReportCompose")
+            connection.set("scm:git:git://github.com/otangid/CrashReportCompose.git")
+            developerConnection.set("scm:git:ssh://github.com/otangid/CrashReportCompose.git")
+        }
+    }
 }
